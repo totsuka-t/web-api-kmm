453e89f126210e88865ccc5b34eee08af9795d6d
totsuka <putti0702@gmail.com>
2024-02-09 10:36:44 +0900

    Implement a list of WebAPI

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,10 +1,23 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -14,45 +27,51 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.key.Key.Companion.R
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
-@OptIn(ExperimentalResourceApi::class)
+
 @Composable
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        val greeting = remember { Greeting().greet() }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = {
-                showContent = !showContent
-            }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource("compose-multiplatform.xml"), null)
-                    Text("Compose: $greeting")
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = { Text("Case Studies") },
+                    )
                 }
-            }
-            val scope = rememberCoroutineScope()
-            var text by remember { mutableStateOf("Loading") }
-            LaunchedEffect(true) {
-                scope.launch {
-                    text = try {
-                        NumbersAPIClient().fetchTrivia(5)
-                    } catch (e: Exception) {
-                        "error: $e"
+            ) {
+                Column {
+                    ListTitle("NumbersAPI") {
+                        println("API called NumbersAPI")
                     }
                 }
             }
-            GreetingView(text)
         }
     }
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+private fun ListTitle(title: String, onClick: () -> Unit) {
+    Surface(
+        modifier = Modifier.clickable { onClick() },
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(all = 10.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp, vertical = 16.dp),
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.subtitle1,
+            )
+        }
+    }
 }
