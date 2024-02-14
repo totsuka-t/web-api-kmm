6acfb6202f2472c05728e8e794d4dcf6285a9288
totsuka <putti0702@gmail.com>
2024-02-14 21:19:11 +0900

    Implement a WeatherList of the day

--- composeApp/src/commonMain/kotlin/WeatherScreen.kt
+++ composeApp/src/commonMain/kotlin/WeatherScreen.kt
@@ -1,7 +1,10 @@
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
@@ -10,7 +13,11 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
@@ -20,6 +27,8 @@ class WeatherScreen(private val onClick: () -> Unit) : Screen {
 
     @Composable
     override fun Content() {
+        var weather by remember { mutableStateOf<Weather?>(null) }
+
         Scaffold(
             topBar = {
                 TopAppBar(
@@ -36,18 +45,28 @@ class WeatherScreen(private val onClick: () -> Unit) : Screen {
             }
         ) {
             Column(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
+                weather?.let {
+                    for (i in it.hourly.time.indices) {
+                        Text("time: ${it.hourly.time[i]}")
+                        Text("temperature: ${it.hourly.temperature2m[i]}")
+                        Text("precipitationProbability: ${it.hourly.precipitationProbability[i]}")
+                        Divider()
+                    }
+                }
+
                 val scope = rememberCoroutineScope()
-                Text("WeatherScreen")
                 Button({
                     scope.launch {
-                        WeatherAPIClient().fetchWeather(52.52, 13.41, "Asia%2FTokyo", 1)
+                        weather = WeatherAPIClient().fetchWeather(52.52, 13.41, "Asia%2FTokyo", 1)
                     }
                 }) {
-
+                    Text("fetch weather")
                 }
             }
         }
