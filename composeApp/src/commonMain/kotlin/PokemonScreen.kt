67ddbe32651ae94c1162be7d57fbc3e601fa2f26
totsuka <putti0702@gmail.com>
2024-02-20 21:20:30 +0900

    Implement a Card Composable

--- composeApp/src/commonMain/kotlin/PokemonScreen.kt
+++ composeApp/src/commonMain/kotlin/PokemonScreen.kt
@@ -1,7 +1,10 @@
-
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -13,6 +16,9 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,6 +27,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import io.kamel.image.KamelImage
@@ -63,34 +70,148 @@ class PokemonScreen(private val onClick: () -> Unit) : Screen {
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 pokemon1?.let {
-                    val painterResource = asyncPainterResource(data = it.sprites.frontDefault) {
-                        coroutineContext = Job() + Dispatchers.IO
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        ),
+                        modifier = Modifier
+                            .size(width = 300.dp, height = 180.dp)
+                    ) {
+                        Box {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Column {
+                                    Text(
+                                        text = "[ID: ${it.id}] ${it.name}",
+                                        modifier = Modifier
+                                            .padding(16.dp),
+                                        textAlign = TextAlign.Center,
+                                    )
+                                    Text(
+                                        text = "Height: ${it.height}",
+                                        modifier = Modifier
+                                            .padding(16.dp),
+                                        textAlign = TextAlign.Center,
+                                    )
+                                    Text(
+                                        text = "Weight: ${it.weight}",
+                                        modifier = Modifier
+                                            .padding(16.dp),
+                                        textAlign = TextAlign.Center,
+                                    )
+                                }
+                                val painterResource = asyncPainterResource(data = it.sprites.frontDefault) {
+                                    coroutineContext = Job() + Dispatchers.IO
+                                }
+                                KamelImage(
+                                    painterResource,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(100.dp)
+                                )
+                            }
+                        }
                     }
-                    KamelImage(
-                        painterResource,
-                        contentDescription = null,
-                        modifier =  Modifier.size(200.dp)
-                    )
                 }
+
+                Spacer(Modifier.size(10.dp))
+
                 pokemon2?.let {
-                    val painterResource = asyncPainterResource(data = it.sprites.frontDefault) {
-                        coroutineContext = Job() + Dispatchers.IO
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        ),
+                        modifier = Modifier
+                            .size(width = 300.dp, height = 180.dp)
+                    ) {
+                        Box {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Column {
+                                    Text(
+                                        text = "[ID: ${it.id}] ${it.name}",
+                                        modifier = Modifier
+                                            .padding(16.dp),
+                                        textAlign = TextAlign.Center,
+                                    )
+                                    Text(
+                                        text = "Height: ${it.height}",
+                                        modifier = Modifier
+                                            .padding(16.dp),
+                                        textAlign = TextAlign.Center,
+                                    )
+                                    Text(
+                                        text = "Weight: ${it.weight}",
+                                        modifier = Modifier
+                                            .padding(16.dp),
+                                        textAlign = TextAlign.Center,
+                                    )
+                                }
+                                val painterResource = asyncPainterResource(data = it.sprites.frontDefault) {
+                                    coroutineContext = Job() + Dispatchers.IO
+                                }
+                                KamelImage(
+                                    painterResource,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(100.dp)
+                                )
+                            }
+                        }
                     }
-                    KamelImage(
-                        painterResource,
-                        contentDescription = null,
-                        modifier =  Modifier.size(200.dp)
-                    )
                 }
+
+                Spacer(Modifier.size(10.dp))
+
                 pokemon3?.let {
-                    val painterResource = asyncPainterResource(data = it.sprites.frontDefault) {
-                        coroutineContext = Job() + Dispatchers.IO
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        ),
+                        modifier = Modifier
+                            .size(width = 300.dp, height = 180.dp)
+                    ) {
+                        Box {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Column {
+                                    Text(
+                                        text = "[ID: ${it.id}] ${it.name}",
+                                        modifier = Modifier
+                                            .padding(16.dp),
+                                        textAlign = TextAlign.Center,
+                                    )
+                                    Text(
+                                        text = "Height: ${it.height}",
+                                        modifier = Modifier
+                                            .padding(16.dp),
+                                        textAlign = TextAlign.Center,
+                                    )
+                                    Text(
+                                        text = "Weight: ${it.weight}",
+                                        modifier = Modifier
+                                            .padding(16.dp),
+                                        textAlign = TextAlign.Center,
+                                    )
+                                }
+                                val painterResource = asyncPainterResource(data = it.sprites.frontDefault) {
+                                    coroutineContext = Job() + Dispatchers.IO
+                                }
+                                KamelImage(
+                                    painterResource,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(100.dp)
+                                )
+                            }
+                        }
                     }
-                    KamelImage(
-                        painterResource,
-                        contentDescription = null,
-                        modifier =  Modifier.size(200.dp)
-                    )
                 }
 
                 val scope = rememberCoroutineScope()
